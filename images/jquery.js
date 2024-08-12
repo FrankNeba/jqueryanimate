da3cabe4568b94e5e755bf623f2f61433afb94dd
FrankNeba <ngwafranklin29@gmail.com>
2024-08-12 02:58:02 -0700

    scroll down fixed

--- images/jquery.js
+++ images/jquery.js
@@ -1,156 +1,143 @@
-var tops = true
-let nextpage = false
-var clicked = false
-var animate = false
+var tops = true;
+let nextpage = false;
+var clicked = false;
+var animate = false;
 
-function loadNextPage(){
-    // $('#content').removeClass('hidden');
-    // $('#block1').addClass('hidden');
-    if (tops){
-        window.scrollTo({top: 150, behavior:"smooth"});
-        console.log(tops);
-        tops = false
-    }//scrolls down when next page loads
-              
-    // Text appear animation start
-    // Set id of "LEADING INSIGHT" text to block2
-    $('#leading-text-container').css('display','flex')
-    if(!animate){
-         $("#leading-text").animate(
-        {
-            paddingLeft: "400px"
-          }, 2700 , 'swing',  
-        );
-
-        $('#leading-text').animate({
-            paddingLeft: "50px",
-            },300)
-
-        $('#leading-text-container').animate({
-            marginTop: "-80%",
-            marginLeft: "0%",
-            width: '100%',
-            fontSize: "50px",
-        }, 3000, 'swing')
-
-        setTimeout(() => {
-            $('#leading-text-container').animate({
-            marginTop: "-90%",
-            }, 400)
-
-            $('#leading-text').animate({
-                fontWeight: "700",
-            },300)
-
-            $('#content').removeClass('hidden')
-        }, 500)
-
-        setTimeout(() => {
-            $('#first-section').addClass('hidden')
-            }, 3000)
-        setTimeout(() => {
-        $('#nav-bar').css('color', 'black')
-        // $('#bg').css('paddingTop','60px')
-        }, 2500)
-
-        animate = true
+function loadNextPage() {
+    if (tops) {
+        console.log('Loading next page...');
+        tops = false;
     }
 
-        nextpage = true
-} // load next page ends
-
-
-
-// Load previous page starts
-function loadPreviousPage(){
-
-    
-    if(!animate){
+    // Ensure both animations are synced
+    $('#leading-text-container').css('display', 'flex');
+    if (!animate) {
+        // Animate leading text
         $("#leading-text").animate(
-            {
-                // paddingRight: "800px"
-            }, 2700 , 'swing',  
+            { paddingLeft: "400px" }, 
+            2700, 
+            'swing'
+        );
+        $("#leading-text").animate(
+            { paddingLeft: "0px" }, 
+            300, 
+            'swing'
         );
 
-        // $("#leading-text").animate(
-        //     {
-        //         paddingLeft: "-400px"
-        //     }, 2700 , 'swing',  
-        // );
+        $('#leading-text-container').animate({
+            marginTop: "0px",  // Changed from percentage to pixels
+            marginLeft: "0px", // Changed from percentage to pixels
+            fontSize: "50px",
+        }, 3000, 'swing');
 
-       $('#leading-text-container').animate({
-        marginTop: "-6550px",
-        // marginRight: "800%",
-        marginLeft: "-40%",
-        width: '100%',
-        fontSize: "5500px",
-    }, 2000, 'swing')
+        $('#leading-text').animate({
+            paddingTop: "510px",
+        }, 400);
 
-       setTimeout(() => {
+        // Animate block1 to scroll up and fade out
+        $('#block1').animate({
+            marginTop: "-1200px", // Adjust based on your layout
+            opacity: 0,
+        }, 3000, 'swing');
 
-           $('#content').addClass('hidden')
-           $('#leading-text-container').css('display','none')
-       }, 3000)
+        setTimeout(() => {
+            $('#leading-text').animate({
+                fontWeight: "700",
+            }, 300);
 
-       setTimeout(() => {
-       $('#leading-text-container').css('marginLeft','-1830%')
-    }, 3100)
-       
+            $('#content').removeClass('hidden');
+        }, 500);
 
-       setTimeout(() => {
-           $('#first-section').removeClass('hidden')
-           }, 10)
+        setTimeout(() => {
+            $('#block1').addClass('hidden');
+        }, 3000);
 
-        
-       animate = true
-       tops = true
-       
-       setTimeout(() => {
-       $('#nav-bar').css('color', 'white')
-       }, 100) // change color of links in nav-bar
-    //    setTimeout(() =>{
-        
-    //     $('#leading-text-container').css('marginLeft','300px')
-    //    },100);
-       
-   }
-    
-      $(document).ready(function () {
-        animate=false
+        setTimeout(() => {
+            $('#nav-bar').css('color', 'black');
+        }, 2500);
 
+        animate = true;
+    }
 
-        window.scrollTo({top: 0});
-        $('#downbutton').click(function() {
-            console.log(clicked);
-            clicked = true;
-            console.log(clicked);
-            if ( clicked) {
-               loadNextPage()
-        // Text appear animation end
-            }  
-        });
-    })
+    nextpage = true;
 }
 
+function loadPreviousPage() {
+    if (!animate) {
+        console.log('Loading previous page...');
 
-
-        
-        $(window).scroll(function() {
-            var windowBottom = $(window).scrollTop() + $(window).height();
-           var scrollPosition = $(window).scrollTop() - 4;
-            var targetTop = $('#content').offset().top;
-            
-            if (windowBottom >= targetTop + 800) {
-
-                loadNextPage()
-            }
-            if (scrollPosition < 0 && nextpage) {
-                console.log(scrollPosition);
-                
-                loadPreviousPage()
-
-                nextpage == false
-
-            } 
+        // Ensure the text container is visible and reset its position
+        $('#leading-text-container').css({
+            display: 'flex',
+            marginTop: "-6550px", // Changed from percentage to pixels
+            marginLeft: "-1830px", // Changed from percentage to pixels
+            fontSize: "5500px",
         });
-        
+
+        $('#leading-text').css({
+            paddingTop: "550px",
+            paddingLeft: "0px",
+            paddingBottom: "200px",
+            fontWeight: "1000",
+        });
+
+        // Animate leading text to fade out and scroll up
+        $('#leading-text-container').animate({
+            marginTop: "-6550px", // Changed from percentage to pixels
+            marginLeft: "-1830px", // Changed from percentage to pixels
+            fontSize: "5500px",
+        }, 3000, 'swing');
+
+        $('#leading-text').animate({
+            paddingTop: "550px",
+        }, 400);
+
+        // Animate block1 to scroll down and appear
+        $('#block1').removeClass('hidden').css({ opacity: 0 }); // Ensure block1 is visible before animating
+        $('#block1').animate({
+            marginTop: "0px",
+            opacity: 1,
+        }, 3000, 'swing');
+
+        setTimeout(() => {
+            $('#content').addClass('hidden');
+            $('#leading-text-container').css('display', 'none'); // Hide after animation
+        }, 3000);
+
+        setTimeout(() => {
+            $('#nav-bar').css('color', 'white');
+        }, 2500);
+
+        animate = true;
+        tops = true;
+        nextpage = false; // Reset to allow the next page transition
+    }
+}
+
+$(document).ready(function () {
+    $('#downbutton').click(function() {
+        console.log('Down button clicked');
+        clicked = true;
+        if (clicked && !nextpage) {
+            loadNextPage();
+        }
+    });
+});
+
+$(window).scroll(function() {
+    var windowBottom = $(window).scrollTop() + $(window).height();
+    var targetTop = $('#content').offset().top;
+
+    // Trigger page load when the bottom of the window reaches the target top
+    if (windowBottom >= targetTop && !nextpage) {
+        console.log('Scroll bottom reached, loading next page...');
+        loadNextPage();
+    }
+    
+    // Trigger page load when scrolled to the top
+    var scrollPosition = $(window).scrollTop();
+    if (scrollPosition <= 10 && nextpage) { // Adjust the threshold as needed
+        console.log('Scroll position:', scrollPosition, 'Loading previous page...');
+        loadPreviousPage();
+    }
+});
